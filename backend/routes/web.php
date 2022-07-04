127afe126704dbb2ebce9d03982460d28461fd5c
dharmapradhan2 <dharma.pradhan@nettantra.net>
2022-07-04 13:31:52 +0530

    commit

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MailController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+// Route::get('/email',[MailController::class,'index']);
+
+Route::get('/email', function () {
+    return view('emails.mail');
 });
